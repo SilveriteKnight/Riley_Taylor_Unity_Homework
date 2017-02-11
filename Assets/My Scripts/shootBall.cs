6652fa26220d8bea4f821ae4f58de6b505d3d773
THEHOG96 <riley.taylor@wsu.edu>
2017-02-10 16:46:13 -0800

    Week5C

--- Assets/My Scripts/shootBall.cs
+++ Assets/My Scripts/shootBall.cs
@@ -6,11 +6,12 @@ public class shootBall : MonoBehaviour {
 	public GameObject projectile;
 	public float speed = 20;
 	private GameObject clone;
+	public float padding = 1;
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown("Fire1"))
 		{
-			clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+			clone = Instantiate(projectile, transform.position + (Vector3.forward * padding), transform.rotation) as GameObject;
 			clone.GetComponent<Rigidbody>().AddForce(clone.transform.forward * speed);
 		}
 	}
